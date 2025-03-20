bdaee0a5688659fcf603c6d1d74d1982928f8165
umar7shaikh <umarskpune@gmail.com>
2025-03-20 16:33:00 +0530

    changes in the setting

--- JazarSite/JazarSite/settings.py
+++ JazarSite/JazarSite/settings.py
@@ -196,17 +196,4 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
-import os
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# Email Configuration (Outlook)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.office365.com'  # For Outlook
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
